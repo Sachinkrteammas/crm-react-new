c6f981e4bd181c71da228121fc975039956369d9
Prakhar Gupta <Prakhar.gupta@teammas.co.in>
2026-03-30 16:02:22 +0530

    updates Sla client wise

--- Crm_Backend/SLA_client_wise.py
+++ Crm_Backend/SLA_client_wise.py
@@ -133,7 +133,7 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
     if req.company_id.upper() == "ALL":
         rows = db.execute(
             text(f"""
-                SELECT campaignid, company_id
+                SELECT campaignid, company_id, company_name
                 FROM registration_master
                 WHERE status='A'
                   AND is_dd_client='1'
@@ -145,7 +145,7 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
     else:
         rows = db.execute(
             text(f"""
-                SELECT campaignid, company_id
+                SELECT campaignid, company_id, company_name
                 FROM registration_master
                 WHERE company_id = :cid
                   AND status='A'
@@ -162,11 +162,18 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
     client_campaigns = {}
     for r in rows:
         company_id = r.company_id
+        company_name = r.company_name
         if r.campaignid:
             campaigns = [c.strip().strip("'") for c in r.campaignid.split(",") if c.strip()]
-            client_campaigns[company_id] = campaigns
+            # client_campaigns[company_id] = campaigns
         else:
-            client_campaigns[company_id] = []
+            campaigns = []
+            # client_campaigns[company_id] = []
+
+        client_campaigns[company_id] = {
+            "company_name": company_name,
+            "campaigns": campaigns
+        }
 
     # Fetch plan rates per company
     company_rates = {}
@@ -204,7 +211,7 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
     total_acht = 0
     total_answered = 0
     total_offered = 0
-    rl_list = set()
+    # rl_list = set()
 
 
     grand_totals = {
@@ -219,9 +226,47 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
         "Amount": 0,
         "RL": 0,
         "Call Rate": 0
-    }
+    }   
+        
 
-    for company_id, campaigns in client_campaigns.items():
+    for company_id, info in client_campaigns.items():
+        # ---------------- RL & RL% calculation ----------------
+
+        rl_sql = """
+            SELECT COUNT(1) AS cnt
+            FROM aband_call_master
+            WHERE ClientId = :cid
+            AND call_status = 'answer'
+            AND DATE(calldate) BETWEEN :from_date AND :to_date
+        """
+
+        rl_row = db.execute(
+            text(rl_sql),
+            {
+                "cid": company_id,
+                "from_date": req.from_date,
+                "to_date": req.to_date
+            }
+        ).fetchone()
+
+        company_rl = int(rl_row.cnt or 0)
+
+        company_name = info["company_name"]
+        campaigns = info["campaigns"]
+
+        if company_name not in all_data:
+            all_data[company_name] = {
+                "Client Name": company_name,
+                "Offered": 0,
+                "Handled": 0,
+                "Calls Ans (20 Sec)": 0,
+                "Total Calls Abandoned": 0,
+                "Abnd Within (20)": 0,
+                "Total Talk Time_sec": 0,
+                "AHT_total": 0,
+                "RL": company_rl
+            }
+
         rate_info = company_rates[company_id]
         for campaign in campaigns:
             sql = f"""
@@ -313,74 +358,54 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
             # wi_thin_sla_ten = float(row["WIthinSLATen"] or 0)
             # abnd_within = float(row["AbndWithinThresold"] or 0)
 
-            # ---------------- RL & RL% calculation ----------------
-            rl = 0
-
-            if company_id not in rl_list:
-                rl_sql = """
-                    SELECT COUNT(1) AS cnt
-                    FROM aband_call_master
-                    WHERE ClientId = :cid
-                    AND call_status = 'answer'
-                    AND DATE(calldate) BETWEEN :from_date AND :to_date
-                """
-                rl_row = db.execute(
-                    text(rl_sql),
-                    {
-                        "cid": company_id,
-                        "from_date": req.from_date,
-                        "to_date": req.to_date
-                    }
-                ).fetchone()
-
-                rl = int(rl_row.cnt or 0)
-                rl_list.add(company_id)
-            else:
-                rl = 0
+            
 
             # RL %
             denominator = handled + abandon
-            rl_percent = round(((handled + rl) / denominator) * 100) if denominator > 0 else 0
+            # rl_percent = round(((handled + rl) / denominator) * 100) if denominator > 0 else 0
 
             # -------- FILTER LOGIC --------
             # skips Offered that have 0
-            if req.filter_type == "without_0" and offered <= 0:
-                continue
+            # if req.filter_type == "without_0" and offered <= 0:
+            #     continue
 
-            # grand_totals = {
-            #     "Offered": 0,
-            #     "Handled": 0,
-            #     "Total Talk Time": "00:00:00",
-            #     "Calls Ans (20 Sec)": 0,
-            #     "Calls Ans (10 Sec)": 0,
-            #     "Total Calls Abandoned": 0,
-            #     "Abnd Within (20)": 0,
-            #     "AHT_total_sec": 0,   # for weighted AHT
-            #     "Amount": 0,
-            #     "RL": 0,
-            #     "Call Rate": 0
+            
+
+
+
+            data = all_data[company_name]
+
+            data["Offered"] += offered
+            data["Handled"] += handled
+            data["Calls Ans (20 Sec)"] += wi_thin_sla
+            data["Total Calls Abandoned"] += abandon
+            data["Abnd Within (20)"] += abnd_within
+            data["Total Talk Time_sec"] += talk_time_sec
+            # data["RL"] += rl
+
+            if handled > 0:
+                data["AHT_total"] += total_acht_row
+
+
+            # all_data[campaign] = {
+            #     "Client Name": campaign,
+            #     "Offered": offered,
+            #     "Handled": handled,
+            #     "SL% (20 Sec)": f"{round(wi_thin_sla*100/handled) if handled else 0}%",
+            #     "AL": f"{round(handled*100/offered) if offered else 0}%",
+            #     "Calls Ans (20 Sec)": wi_thin_sla,
+            #     # "Calls Ans (10 Sec)": wi_thin_sla_ten,
+            #     "Total Calls Abandoned": abandon,
+            #     "Abnd Within (20)": abnd_within,
+            #     "Total Talk Time": seconds_to_hms(talk_time_sec),
+            #     "Average Aband Time": "",
+            #     # "SL% (10 Sec)": f"{round(wi_thin_sla_ten*100/handled) if handled else 0}%",               
+            #     "AHT (In Sec)": round(total_acht_row / handled) if handled else 0,
+            #     # "Call Rate": rate_info["InboundCallCharge"],
+            #     # "Amount": round(abandon * float(rate_info["rate_per_sec"]) * round((total_acht_row / handled if handled else 0)), 2),
+            #     "RL": rl,
+            #     "RL%": f"{rl_percent}%"
             # }
-
-
-            all_data[campaign] = {
-                "Client Name": campaign,
-                "Offered": offered,
-                "Handled": handled,
-                "Calls Ans (20 Sec)": wi_thin_sla,
-                "Calls Ans (10 Sec)": wi_thin_sla_ten,
-                "Total Calls Abandoned": abandon,
-                "Abnd Within (20)": abnd_within,
-                "Total Talk Time": seconds_to_hms(talk_time_sec),
-                "Average Aband Time": "",
-                "SL% (20 Sec)": f"{round(wi_thin_sla*100/handled) if handled else 0}%",
-                "SL% (10 Sec)": f"{round(wi_thin_sla_ten*100/handled) if handled else 0}%",
-                "AL": f"{round(handled*100/offered) if offered else 0}%",
-                "AHT (In Sec)": round(total_acht_row / handled) if handled else 0,
-                "Call Rate": rate_info["InboundCallCharge"],
-                "Amount": round(abandon * float(rate_info["rate_per_sec"]) * round((total_acht_row / handled if handled else 0)), 2),
-                "RL": rl,
-                "RL%": f"{rl_percent}%"
-            }
 
             # Accumulate totals safely as float
             total_handle += total_acht_row
@@ -388,8 +413,27 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
             total_answered += handled
             total_offered += offered
 
+    if req.filter_type == "without_0":
+        all_data = {
+            k: v for k, v in all_data.items()
+            if v["Offered"] > 0
+        }
 
 
+
+    for company_name, data in all_data.items():
+        handled = data["Handled"]
+        offered = data["Offered"]
+
+        data["Total Talk Time"] = seconds_to_hms(data["Total Talk Time_sec"])
+
+        data["SL% (20 Sec)"] = f"{round(data['Calls Ans (20 Sec)'] * 100 / handled) if handled else 0}%"
+        data["AL"] = f"{round(handled * 100 / offered) if offered else 0}%"
+
+        data["AHT (In Sec)"] = round(data["AHT_total"] / handled) if handled else 0
+
+        denominator = handled + data["Total Calls Abandoned"]
+        data["RL%"] = f"{round((handled + data['RL']) * 100 / denominator) if denominator else 0}%"
 
 
     # -------------------- Generate Excel --------------------
@@ -397,10 +441,16 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
     ws = wb.active
     ws.title = "SLA Clientwise Report"
 
+    # headers = [
+    #     "Client Name", "Offered", "Handled", "Calls Ans (20 Sec)", "Calls Ans (10 Sec)",
+    #     "Total Calls Abandoned", "Abnd Within (20)", "Average Aband Time", "Total Talk Time", "SL% (20 Sec)", 
+    #     "SL% (10 Sec)", "AL", "AHT (In Sec)", "Call Rate", "Amount", "RL", "RL%"
+    # ]
+
     headers = [
-        "Client Name", "Offered", "Handled", "Calls Ans (20 Sec)", "Calls Ans (10 Sec)",
-        "Total Calls Abandoned", "Abnd Within (20)", "Average Aband Time", "Total Talk Time", "SL% (20 Sec)", 
-        "SL% (10 Sec)", "AL", "AHT (In Sec)", "Call Rate", "Amount", "RL", "RL%"
+        "Client Name", "Offered", "Handled", "SL% (20 Sec)", "AL", "Calls Ans (20 Sec)",
+        "Total Calls Abandoned", "Abnd Within (20)", "Total Talk Time",  
+        "AHT (In Sec)", "RL", "RL%"
     ]
     fill = PatternFill(start_color="317EAC", end_color="317EAC", fill_type="solid")
     font = Font(color="FFFFFF", bold=True)
@@ -412,7 +462,9 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
 
     last_data_row = 1
 
-    for row_num, (campaign, data) in enumerate(all_data.items(), start=2):
+    sorted_data = dict(sorted(all_data.items(), key=lambda x: x[0]))
+
+    for row_num, (company, data) in enumerate(sorted_data.items(), start=2):
         last_data_row = row_num        
 
         for col_num, key in enumerate(headers, start=1):
@@ -429,12 +481,12 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
         grand_totals["Total Talk Time_sec"] += timedelta_to_seconds(data["Total Talk Time"])
 
         grand_totals["Calls Ans (20 Sec)"] += data["Calls Ans (20 Sec)"]
-        grand_totals["Calls Ans (10 Sec)"] += data["Calls Ans (10 Sec)"]
+        # grand_totals["Calls Ans (10 Sec)"] += data["Calls Ans (10 Sec)"]
         grand_totals["Total Calls Abandoned"] += data["Total Calls Abandoned"]
         grand_totals["Abnd Within (20)"] += data["Abnd Within (20)"]
-        grand_totals["Average Aband Time"] = 0
-        grand_totals["Call Rate"] += data["Call Rate"]
-        grand_totals["Amount"] += data["Amount"]
+        # grand_totals["Average Aband Time"] = 0
+        # grand_totals["Call Rate"] += data["Call Rate"]
+        # grand_totals["Amount"] += data["Amount"]
         grand_totals["RL"] += data["RL"]
 
         if handled > 0:
@@ -490,19 +542,21 @@ def sla_clientwise_report_excel(req: SLAClientwiseReq, db2: Session = Depends(ge
 
     ws.cell(row=grand_row, column=col_map["Offered"], value=grand_totals["Offered"])
     ws.cell(row=grand_row, column=col_map["Handled"], value=grand_totals["Handled"])
+    ws.cell(row=grand_row, column=col_map["SL% (20 Sec)"], value=f'{grand_totals["SL% (20 Sec)"]}%')
+    ws.cell(row=grand_row, column=col_map["AL"], value=f'{grand_totals["AL"]}%')
     ws.cell(row=grand_row, column=col_map["Total Talk Time"], value=seconds_to_hms(grand_totals["Total Talk Time_sec"]))
     ws.cell(row=grand_row, column=col_map["Calls Ans (20 Sec)"], value=grand_totals["Calls Ans (20 Sec)"])
-    ws.cell(row=grand_row, column=col_map["Calls Ans (10 Sec)"], value=grand_totals["Calls Ans (10 Sec)"])
+    # ws.cell(row=grand_row, column=col_map["Calls Ans (10 Sec)"], value=grand_totals["Calls Ans (10 Sec)"])
     ws.cell(row=grand_row, column=col_map["Total Calls Abandoned"], value=grand_totals["Total Calls Abandoned"])
     ws.cell(row=grand_row, column=col_map["Abnd Within (20)"], value=grand_totals["Abnd Within (20)"])
-    ws.cell(row=grand_row, column=col_map["Average Aband Time"], value=grand_totals["Average Aband Time"])
-    ws.cell(row=grand_row, column=col_map["Call Rate"], value=grand_totals["Call Rate"])
-    ws.cell(row=grand_row, column=col_map["Amount"], value=round(grand_totals["Amount"], 2))
+    # ws.cell(row=grand_row, column=col_map["Average Aband Time"], value=grand_totals["Average Aband Time"])
+    # ws.cell(row=grand_row, column=col_map["Call Rate"], value=grand_totals["Call Rate"])
+    # ws.cell(row=grand_row, column=col_map["Amount"], value=round(grand_totals["Amount"], 2))
     ws.cell(row=grand_row, column=col_map["RL"], value=grand_totals["RL"])
     ws.cell(row=grand_row, column=col_map["RL%"], value=f'{grand_totals["RL%"]}%')
-    ws.cell(row=grand_row, column=col_map["SL% (20 Sec)"], value=f'{grand_totals["SL% (20 Sec)"]}%')
-    ws.cell(row=grand_row, column=col_map["SL% (10 Sec)"], value=f'{grand_totals["SL% (10 Sec)"]}%')
-    ws.cell(row=grand_row, column=col_map["AL"], value=f'{grand_totals["AL"]}%')
+    
+    # ws.cell(row=grand_row, column=col_map["SL% (10 Sec)"], value=f'{grand_totals["SL% (10 Sec)"]}%')
+   
 
 
     # Weighted AHT
