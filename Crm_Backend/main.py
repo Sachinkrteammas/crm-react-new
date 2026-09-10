b16441ef97acea20d1ef21863445032075162804
Prakhar Gupta <Prakhar.gupta@teammas.co.in>
2026-09-10 12:19:59 +0530

    run schedular

--- Crm_Backend/main.py
+++ Crm_Backend/main.py
@@ -346,11 +346,11 @@ scheduler.add_job(pull_salesforce_leads, "interval", minutes=5)
 # scheduler.add_job(run_push_to_sheet, "interval", minutes=1, max_instances=1)
 #scheduler.add_job(run_sla_push_to_sheet, "interval", minutes=1, max_instances=1)
 scheduler.add_job(run_abandoned_call_sms, "interval", minutes=1)
-# scheduler.add_job(scheduled_pending_alerts, "interval", minutes=1, max_instances=1)
+scheduler.add_job(scheduled_pending_alerts, "interval", minutes=1, max_instances=1)
 scheduler.add_job(sync_recordings, "cron", hour=1, minute=30)
 scheduler.add_job(get_call_followups, "interval", minutes=30)
-# scheduler.add_job(scheduled_escalation_checks, "interval", minutes=1, max_instances=1)
-# scheduler.add_job(scheduled_close_loop_checks, "interval", minutes=1, max_instances=1)  # close-loop SMS alerts
+scheduler.add_job(scheduled_escalation_checks, "interval", minutes=1, max_instances=1)
+scheduler.add_job(scheduled_close_loop_checks, "interval", minutes=1, max_instances=1)  # close-loop SMS alerts
 
 
 
